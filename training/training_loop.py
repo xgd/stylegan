94bc06f2174afef703912af088ffa9ccd442c987
xduan <xg.d@aol.com>
2019-07-08 10:41:16 +0800

    params for stanford cars

--- training/training_loop.py
+++ training/training_loop.py
@@ -134,7 +134,7 @@ def training_loop(
     D_repeats               = 1,        # How many times the discriminator is trained per G iteration.
     minibatch_repeats       = 4,        # Number of minibatches to run before adjusting training parameters.
     reset_opt_for_new_lod   = True,     # Reset optimizer internal state (e.g. Adam moments) when new layers are introduced?
-    total_kimg              = 7991,    # Total length of the training, measured in thousands of real images.
+    total_kimg              = 16185,    # Total length of the training, measured in thousands of real images.
     mirror_augment          = False,    # Enable mirror augment?
     drange_net              = [-1,1],   # Dynamic range used when feeding image data to the networks.
     image_snapshot_ticks    = 1,        # How often to export image snapshots?
@@ -143,7 +143,7 @@ def training_loop(
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
     resume_run_id           = "/kaggle/working/stylegan/network-snapshot-007961.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
-    resume_kimg             = 7961,      # Assumed training progress at the beginning. Affects reporting and training schedule.
+    resume_kimg             = 0.0,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
 
     # Initialize dnnlib and TensorFlow.
