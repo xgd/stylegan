b69a1901d88125ff985b948445db359b96d6a138
xduan <xg.d@aol.com>
2019-07-08 14:08:37 +0800

    find tune model from google drive

--- training/training_loop.py
+++ training/training_loop.py
@@ -141,7 +141,7 @@ def training_loop(
     network_snapshot_ticks  = 1,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = "/kaggle/working/stylegan/network-snapshot-007961.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = "/content/drive/My Drive/karras2019stylegan-cars-512x384.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
     resume_kimg             = 0.0,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
