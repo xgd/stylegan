dfbdb069333f50f9110fe8f9a0d28f28c4d337ac
xduan <xg.d@aol.com>
2019-07-09 18:46:14 +0800

    add import

--- training/dataset.py
+++ training/dataset.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 import dnnlib
 import dnnlib.tflib as tflib
 import io
+from PIL import Image
 
 #----------------------------------------------------------------------------
 # Parse individual image from a tfrecords file.
