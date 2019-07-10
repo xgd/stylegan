932355a2190dd30111472e9f3690a11b00b67181
xduan <xg.d@aol.com>
2019-07-10 11:56:05 +0800

    fine tune for kaggle

--- training/training_loop.py
+++ training/training_loop.py
@@ -134,16 +134,16 @@ def training_loop(
     D_repeats               = 1,        # How many times the discriminator is trained per G iteration.
     minibatch_repeats       = 4,        # Number of minibatches to run before adjusting training parameters.
     reset_opt_for_new_lod   = True,     # Reset optimizer internal state (e.g. Adam moments) when new layers are introduced?
-    total_kimg              = 16185,    # Total length of the training, measured in thousands of real images.
+    total_kimg              = 15000,    # Total length of the training, measured in thousands of real images.
     mirror_augment          = False,    # Enable mirror augment?
     drange_net              = [-1,1],   # Dynamic range used when feeding image data to the networks.
     image_snapshot_ticks    = 1,        # How often to export image snapshots?
     network_snapshot_ticks  = 1,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = "/content/drive/My Drive/karras2019stylegan-cars-512x384.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = "/kaggle/working/stylegan/karras2019stylegan-cars-512x384.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
-    resume_kimg             = 0.0,      # Assumed training progress at the beginning. Affects reporting and training schedule.
+    resume_kimg             = 7000,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
 
     # Initialize dnnlib and TensorFlow.
