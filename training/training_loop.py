131965d68ccf4f72ce6a9e0b29ccd745c1d91c88
xduan <xg.d@aol.com>
2019-07-11 14:54:44 +0800

    resume latest

--- training/training_loop.py
+++ training/training_loop.py
@@ -155,7 +155,7 @@ def training_loop(
     network_snapshot_ticks  = 1,       # How often to export network snapshots?
     save_tf_graph           = False,    # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,    # Include weight histograms in the tfevents file?
-    resume_run_id           = "/kaggle/working/stylegan/karras2019stylegan-cars-512x384.pkl",     # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = "latest",     # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = None,     # Snapshot index to resume training from, None = autodetect.
     resume_kimg             = 7000,      # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):     # Assumed wallclock time at the beginning. Affects reporting.
